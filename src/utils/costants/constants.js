b573b8b5b046aff9738a43c2e14a0188d69327af
Moloca Adrian <adrian.moloca@smartboxdigital.com>
2022-11-04 12:37:01 +0200

    fix packages

--- src/utils/costants/constants.js
+++ src/utils/costants/constants.js
@@ -1,7 +1,7 @@
-export const adminUrl = 'https://king-prawn-app-vrj9e.ondigitalocean.app/waitero/api/admins/'
-export const clientsUrl = 'https://king-prawn-app-vrj9e.ondigitalocean.app/waitero/api/clients/'
+export const adminUrl = 'http://127.0.0.1:5050/waitero/api/admins/'
+export const clientsUrl = 'http://127.0.0.1:5050/waitero/api/clients/'
 
-export const baseUrl = 'https://king-prawn-app-vrj9e.ondigitalocean.app/waitero/api/'
+export const baseUrl = 'http://127.0.0.1:5050/waitero/api/'
 
 export const restaurant_categories = [ "Traditional", "Pizza", "Burger", "Fast-Food", "Ciorbe/Supe", "Desert", "Aperitive", "Asiatic", "Buritto", "Bauturi", "Cofetarie", "Patiserie", "Fructe de mare", "Gratar", "Noodles", "Paste", "Peste", "Salate", "Sandwich", "Sushi", "Taco", "Vegan", "Vegetarian", "Kebab" ]
 export const drink_categories = [ "Juice", "Coffee", "Wine", "Cognac", "Whiskey", "Vodka", "Shoots", "Water", "Beer" ]
